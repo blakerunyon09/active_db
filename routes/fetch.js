72e0c71346057ee96160e6c864c7a46bf2d8b957
Blake Runyon <blakerunyon09@gmail.com>
2021-06-05 15:20:30 -0600

    Updates on /api/ route validations

--- routes/fetch.js
+++ routes/fetch.js
@@ -25,7 +25,7 @@ router.get('/', (req, res) => {
           ...season
         }
       })
-    res.send(ar)
+    res.send(ar) 
     })
   .catch(function (error) {
     res.send({error: error.data});
